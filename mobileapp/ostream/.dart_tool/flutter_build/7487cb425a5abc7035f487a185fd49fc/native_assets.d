62f6ac5c22f6b9a01b79da76a07abb694b78c74c
Tameem Mahmoud <69875651+TameemMahmoud@users.noreply.github.com>
2024-11-19 11:32:50 +0400

    Delete mobileapp/ostream directory

--- mobileapp/ostream/.dart_tool/flutter_build/7487cb425a5abc7035f487a185fd49fc/native_assets.d
+++
@@ -1,1 +0,0 @@
- /Users/tameem/StudioProjects/ostream/.dart_tool/flutter_build/7487cb425a5abc7035f487a185fd49fc/native_assets.yaml: 
--- mobileapp/ostream/ios/Pods/Target Support Files/babstrap_settings_screen/babstrap_settings_screen-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_babstrap_settings_screen : NSObject
-@end
-@implementation PodsDummy_babstrap_settings_screen
-@end
